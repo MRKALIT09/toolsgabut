81369788df5467ce0ff6d4c86f21159fbd72ee91
MR_KALIT09 <75316440+MRKALIT09@users.noreply.github.com>
2021-01-08 01:08:54 +0700

    Add files via upload

---
+++ tolol.sh
@@ -0,0 +1,83 @@
+#!bin/bash
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+echo
+sleep 2
+echo "$cy [•]═════════════════════════════════════[•]"
+sleep 2
+echo "$ku    ︻̷̿┻̿═━••    AUTHOR : MR KALIT09"
+sleep 2
+echo "$ku    ︻̷̿┻̿═━••  WHATSAAP:0822-7780-4293"
+sleep 2
+echo "$ku    ︻̷̿┻̿═━••https://github.com/MRKALIT09"
+sleep 2
+echo "$cy [•]═════════════════════════════════════[•]"
+sleep 2
+echo "$i___________________________"
+sleep 2
+echo "$cy  MASUKAN PILIHAN MU"
+sleep 2
+echo "$ku 1•     Santet Online"
+sleep 2
+echo "$ku  2•  Exit TOOLS GABUT INI"
+sleep 2
+echo "$i___________________________"
+read -p " =======> " pill
+if [ $pill = "1" ]
+then
+echo -n $me "︻┻̿═━••MASUKAN NAMA MANTAN MU : "
+read t
+echo -n $me "︻┻̿═━••SANTET BAGIAN MANA TOL : "
+read b
+echo -n $me "︻┻̿═━••SENJATA APA? SAPU? SENDOK? PARANG?"
+read s
+echo -n $me "︻┻̿═━•• Klik Enter Tol"
+read e
+clear
+echo $i "SI $t SEDANG DI SANTET TOL AWOKAWOKAWOK"
+echo
+sleep 6
+echo $cy"Tunggu Dlu Tol Si $t Lagi Tusuk Pake $s Di Bagian $b"
+sleep 5
+echo""
+echo $ku"Kek Nya  Si $t Sakit Parah di Bagian $b"
+echo
+sleep 5
+echo $me "Gw Jadi Ngeri Anjg Dia Kek  Anjg gila"
+echo
+sleep 7
+echo $cy " Njir Kek Cacing Kepanasan aowkaowkwowk"
+echo
+sleep 6
+echo $cy "Dah Lah Kasihan Dia Kek orang Gila"
+sleep 4
+echo ""
+sleep 6
+echo $i "Lu Ga Ngecek Dia Kerumah Si $t Kasihan dia Tadi Di Santet Di Bagian  $b"
+sleep 6
+echo ""
+echo ""
+figlet "PEMBUNUH :v"
+echo ""
+exit
+fi
+#batas gan
+if [ $pill = 2 ]
+then
+sleep 3
+figlet "DAH SIAP?"
+sleep 2
+figlet "Jangan Lupa"
+sleep 2
+figlet "MAMPIR"
+sleep 2
+figlet "Lagi"
+exit
+fi
+
